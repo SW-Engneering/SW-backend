cfc215ca18b955ed66b9b8932ce618ef5af8654a
sumin <jss05157@naver.com>
2024-11-19 22:22:14 +0900

    게시글 상세 조회 기능

--- src/main/java/com/swengineer/sportsmatch/repository/BoardRepository.java
+++ src/main/java/com/swengineer/sportsmatch/repository/BoardRepository.java
@@ -2,9 +2,16 @@ package com.swengineer.sportsmatch.repository;
 
 import com.swengineer.sportsmatch.entity.BoardEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<BoardEntity, Long> {
+
+    @Modifying
+    @Query(value = "update BoardEntity b set b.post_hits=b.post_hits+1 where b.post_id=:post_id")
+    void updateHits(@Param("post_id") Long post_id);
 
 }
